fbadd6151f28d35156cb439851baa06dfe8c6535
Maciej Walkowiak <maciej.walkowiak@codeahead.co>
2013-05-22 16:51:04 +0200

    initial project import

---
+++ src/main/resources/archetype-resources/src/main/java/HelloServlet.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(value="/hello", name="helloServlet")
+public class HelloServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("Hello world!!");
+    }
+}
